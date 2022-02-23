522ef95e7da46f0d2637630804a491ea46c5bbce
ArnaudRm <arnaud.ayrem@gmail.com>
2022-02-23 14:17:02 +0100

    Added userExistsMiddleware on group invite to prevent inviting non existing user

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const groupsController = require('../controllers/groups');
 const authMiddleware = require('../middlewares/authentication');
+const userExistsMiddleware = require('../middlewares/userExists');
 
 router.get("/", authMiddleware, groupsController.getGroups);
 router.post("/", authMiddleware, groupsController.createGroup);
-router.post("/:groupId/invite", authMiddleware, groupsController.inviteToGroup);
+router.post("/:groupId/invite", [authMiddleware, userExistsMiddleware], groupsController.inviteToGroup);
 router.get("/latest", authMiddleware, groupsController.getLatestGroup);
 
 module.exports = router;
